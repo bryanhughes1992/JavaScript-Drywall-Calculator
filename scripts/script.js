0274b415f0e66c310d3da429c9595c7d3e598e94
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-12-15 02:39:32 -0500

    Rough Copy - Working Version

--- scripts/script.js
+++ scripts/script.js
@@ -13,38 +13,61 @@ window.onload = function() {
     hiddenContent.style.display = "none";
 
     //Listen for the calculate button being clicked
-    submitBtn.addEventListener("click", function(){
-        drywallForm.submit();
-    });
+    submitBtn.addEventListener("click", calculateSheets);
 
     //Listen for the calculate button being clicked
-    drywallForm.addEventListener("submit", calculateSheets);
 
-    function calculateSheets() {
+
+    function calculateSheets(calcLength, calcWidth) {
+
         //Set the first parameter the value of the length input field
-        var calcLength = drywallForm.wallLength.value;
+        calcLength = drywallForm.wallLength.value;
+
         Math.ceil(parseFloat(calcLength));
+
         //Set the second parameter to the value of the width input field
-        var calcWidth = drywallForm.wallWidth.value;
+        calcWidth = drywallForm.wallWidth.value;
+
         Math.ceil(parseFloat(calcWidth));
+
         //If the wall's length is
-        if (calcLength < 0 || calcLength === "" || isNaN(calcLength)) {
+        if (calcLength <= 0 || calcLength === null || calcLength === undefined || calcLength === "" || isNaN(calcLength)) {
+
             alert("Please input a valid length");
+
             focus(drywallForm.wallLength);
+
             return;
-        } else if ( calcWidth === "" || calcWidth === null ) {
+
+        } else if ( calcWidth <= 0 || calcWidth === null || calcWidth === undefined || isNaN(calcWidth)) {
+
             alert("Please enter a valid width");
+
             focus(drywallForm.wallWidth);
+
+            drywallForm.wallWidth.style.backgroundColor = "pink";
+
             return;
+
         } else {
+
             var wallSqFootage = calcLength * calcWidth;
+
             var amountOfSheets = wallSqFootage / 32;
+
             var sheetString = `${amountOfSheets} sheets of drywall are needed for this wall`;
+
             drywallForm.style.display = "none";
+
             hiddenContent.style.display = "flex";
+
             hiddenContent.style.flexFlow = "column wrap";
+
             hiddenContent.innerHTML = sheetString;
-            console.log(sheetString);
+
+
+            return;
         }
+
     }
 };

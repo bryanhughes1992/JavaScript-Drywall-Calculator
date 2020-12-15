bc34802cf140924686c302adcf6cbcf396da1a9e
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-12-15 02:17:22 -0500

    Rough Copy - Not Working

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,50 @@
 window.onload = function() {
+
     //Capture the entire drywall form as a variable
-    var drywallForm = document.forms.drywallForm;
+    var drywallForm = document.forms[0];
+
     //Capture the submit button as a variable
     var submitBtn = document.getElementById('submitBtn');
+
     //Capture the hidden content as a variable
     var hiddenContent = document.getElementById('hiddenContent');
+
     //Hide the hidden content
     hiddenContent.style.display = "none";
-    //Listen for the drywall form being submitted
-    submitBtn.addEventListener("click", calculateSheets);
 
-    function calculateSheets(wallLength, wallWidth) {
+    //Listen for the calculate button being clicked
+    submitBtn.addEventListener("click", function(){
+        drywallForm.submit();
+    });
+
+    //Listen for the calculate button being clicked
+    drywallForm.addEventListener("submit", calculateSheets);
+
+    function calculateSheets() {
+        //Set the first parameter the value of the length input field
+        var calcLength = drywallForm.wallLength.value;
+        Math.ceil(parseFloat(calcLength));
+        //Set the second parameter to the value of the width input field
+        var calcWidth = drywallForm.wallWidth.value;
+        Math.ceil(parseFloat(calcWidth));
         //If the wall's length is
-        if (wallLength < "0" || wallLength === "" || wallLength === null) {
-            alert("Please input a positive number");
-            focus(wallLength);
+        if (calcLength < 0 || calcLength === "" || isNaN(calcLength)) {
+            alert("Please input a valid length");
+            focus(drywallForm.wallLength);
             return;
-        } else if (wallWidth === "" || wallWidth < "0" || wallWidth === null) {
-            alert("test");
+        } else if ( calcWidth === "" || calcWidth === null ) {
+            alert("Please enter a valid width");
+            focus(drywallForm.wallWidth);
             return;
         } else {
+            var wallSqFootage = calcLength * calcWidth;
+            var amountOfSheets = wallSqFootage / 32;
+            var sheetString = `${amountOfSheets} sheets of drywall are needed for this wall`;
             drywallForm.style.display = "none";
             hiddenContent.style.display = "flex";
-            hiddenContent.style.flexFlow = "column";
-            //Set the first parameter the value of the length input field
-            wallLength = document.drywallForm.wallLength.value;
-            //Set the second parameter to the value of the width input field
-            wallWidth = document.drywallForm.wallWidth.value;
-            //The wall's square footage is equal to this
-            var wallSquareFootage = wallLength * wallWidth;
-            //The amount of sheets needed is equal the wall's square footage divided by the square footage of the drywall sheet
-            var amountOfSheets = wallSquareFootage / 32;
-            //Set a hardcoded string to the display this message
-            var sheetString = `You need ${amountOfSheets} sheets for this wall! `;
-            hiddenContent.innerText = sheetString;
-            return;
+            hiddenContent.style.flexFlow = "column wrap";
+            hiddenContent.innerHTML = sheetString;
+            console.log(sheetString);
         }
     }
 };
